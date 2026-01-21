afda4dc256abc3dfe738ead87ba4e1f45377e506
RoniPG <ronigmn@gmail.com>
2026-01-21 23:08:51 +0100

    feat: validate alarm time format using HH:MM

--- src/main.py
+++ src/main.py
@@ -2,11 +2,17 @@ import random
 import time
 import webbrowser
 
-from datetime import datetime 
+from datetime import datetime
 from pathlib import Path
 
 
 alarm_time = input("Introduce la hora de la alarma (HH:MM) ")
+
+try:
+    datetime.strptime(alarm_time, "%H:%M")
+except ValueError:
+    print("Formato inválido, Usa HH:MM (24h)")
+    exit(1)
 
 ruta_links = Path("data/youtube_links.txt")
 links = ruta_links.read_text().splitlines()
@@ -21,31 +27,3 @@ while True:
         print("¡Despertador activado! Abriendo enlace...")
         break
     time.sleep(30)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
